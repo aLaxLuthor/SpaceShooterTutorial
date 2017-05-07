19c27bc1898be3e2fee2f6f36194005d6174e75b
aLaxLuthor <mathewfraser@gmail.com>
2017-05-07 22:24:29 +1000

    First Commit
    Background, Lighting, Camera, Player Movement.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public Boundary boundary;
+    public float tilt;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {        
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // FixedUpdate is called before each fixed physics step
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(moveHorizontal, 0.0f, moveVertical) * speed;
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax)
+            ,0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
